2bc5c190ea7cb9ef8286df4ec1730fe2f4445761
Hubert Dąbrowski <hubsdab@gmail.com>
2021-06-23 15:29:03 +0200

    Initial desktop app

---
+++ IoT-System-Desktop/IoT-System/Model/ConfigParams.cs
@@ -0,0 +1,87 @@
+﻿using System.IO;
+using System;
+using System.Diagnostics;
+using Newtonsoft.Json;
+
+namespace MultiWPFApp.Model
+{
+    public class ConfigParams
+    {
+        private ConfigData configData;
+        static readonly string ipAddressDefault = "192.168.1.104";
+        public string Url;
+        static readonly int sampleTimeDefault = 500;
+        public int SampleTime;
+        public readonly int MaxSampleNumber = 100;
+
+        static readonly string apiDefault = "20";
+        public string Api;
+
+        static readonly string portDefault = "80";
+        public string Port;
+        private void LoadConfig()
+        {
+            using (StreamReader r = new StreamReader("config.json"))
+            {
+                string json = r.ReadToEnd();
+                configData = JsonConvert.DeserializeObject<ConfigData>(json);
+            }
+        }
+        public double XAxisMax
+        {
+            get
+            {
+                return MaxSampleNumber * SampleTime / 1000.0;
+            }
+            private set { }
+        }
+        public ConfigParams()
+        {
+            try
+            {
+                LoadConfig();
+                if (configData != null)
+                {
+                    Url = configData.url;
+                    SampleTime = configData.sample_time;
+                    Api = configData.api;
+                    Port = configData.port;
+                }
+                else
+                {
+                    Url = ipAddressDefault;
+                    SampleTime = sampleTimeDefault;
+                    Api = apiDefault;
+                    Port = portDefault;
+                }
+            }
+            catch (Exception e)
+            {
+                Url = ipAddressDefault;
+                SampleTime = sampleTimeDefault;
+                Api = apiDefault;
+                Port = portDefault;
+                Debug.WriteLine(e);
+            }
+        }
+
+        public ConfigParams(string ip, int st)
+        {
+            try
+            {
+                LoadConfig();
+                Port = configData.port;
+                Api = configData.api;
+            }
+            catch (Exception e)
+            {
+                Api = apiDefault;
+                Port = portDefault;
+                Debug.WriteLine(e);
+            }
+            Url = ip;
+            SampleTime = st;
+
+        }
+    }
+}
---
+++ IoT-System-Desktop/IoT-System/ViewModel/View1_ViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+
+namespace MultiWPFApp.ViewModel
+{
+    public class View1_ViewModel : BaseViewModel
+    {
+        public Action<object> CodeBehindHandler{ get; set; }
+
+        public SolidColorBrush[,] model = new SolidColorBrush[8, 8];
+
+        public ButtonCommandWithParameter CommonButtonCommand { get; set; }
+
+        public View1_ViewModel()
+        {
+            for(int i=0; i<8; i++)
+            {
+                for(int j=0; j<8; j++)
+                {
+                    model[i, j] = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0));
+                }
+            }
+            CommonButtonCommand = new ButtonCommandWithParameter(SetButtonColor);
+        }
+        private void SetButtonColor(string param)
+        {
+            CodeBehindHandler(param);
+        }
+    }
+}
