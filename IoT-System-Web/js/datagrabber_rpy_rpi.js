77901695322b8cd652814ce4abbfc6a6848b2394
myszol321 <myszola321@wp.pl>
2021-06-23 22:40:09 +0200

    web update

---
+++ IoT-System-Web/js/datagrabber_rpy_rpi.js
@@ -0,0 +1,158 @@
+var sampleTimeSec;                  ///< sample time in sec
+var sampleTimeMsec;  ///< sample time in msec
+var maxSamplesNumber;               ///< maximum number of samples
+
+var xdata; ///< x-axis labels array: time stamps
+var ydataR; ///< y-axis data array: Roll
+var ydataP; ///< y-axis data array: Pitch
+var ydataY; ///< y-axis data array: Yaw
+var lastTimeStamp; ///< most recent time stamp 
+
+var chartContext;  ///< chart context i.e. object that "owns" chart
+var chart;         ///< Chart.js object
+
+var timer; ///< request timer
+
+const url = 'http://192.168.0.101/orientation.dat'; ///< server app with RPY orientation in JSON format
+const get_conf_url = 'http://192.168.0.101/get_config.php';
+function addData(y){
+	if(ydataR.length > maxSamplesNumber)
+	{
+		removeOldData();
+		lastTimeStamp += sampleTimeSec;
+		xdata.push(lastTimeStamp.toFixed(4));
+	}
+	ydataR.push(y["roll"]);
+	ydataP.push(y["pitch"]);
+	ydataY.push(y["yaw"]);
+	chart.update();
+}
+
+function removeOldData(){
+	xdata.splice(0,1);
+	ydataR.splice(0,1);
+	ydataP.splice(0,1);
+	ydataY.splice(0,1);
+}
+
+function startTimer(){
+	timer = setInterval(ajaxJSON, sampleTimeMsec);
+}
+
+function stopTimer(){
+	clearInterval(timer);
+}
+
+function ajaxJSON() {
+	$.ajax(url, {
+		type: 'GET', dataType: 'json',
+		success: function(responseJSON, status, xhr) {
+			addData(responseJSON);
+		}
+	});
+}
+
+function loadConf(data){
+	sampleTimeMsec = data["sample_time"];  ///< sample time in msec
+	sampleTimeSec = sampleTimeMsec/1000;
+	maxSamplesNumber = data["sample_amount"];       ///< maximum number of samples
+	$("#sampletime").text(sampleTimeMsec);
+	$("#samplenumber").text(maxSamplesNumber);
+}
+
+/**
+* @brief Chart initialization
+*/
+function chartInit()
+{
+	$.ajax(get_conf_url, {
+		type: 'GET', dataType: 'json',
+		success: function(responseJSON){
+			loadConf(responseJSON);
+		}
+	})
+	// array with consecutive integers: <0, maxSamplesNumber-1>
+	xdata = [...Array(maxSamplesNumber).keys()]; 
+	// scaling all values ​​times the sample time 
+	xdata.forEach(function(p, i) {this[i] = (this[i]*sampleTimeSec).toFixed(4);}, xdata);
+
+	// last value of 'xdata'
+	lastTimeStamp = +xdata[xdata.length-1]; 
+
+	// empty array
+	ydataR = []; 
+	ydataP = []; 
+	ydataY = []; 
+
+	// get chart context from 'canvas' element
+	chartContext = $("#chart")[0].getContext('2d');
+
+	chart = new Chart(chartContext, {
+		// The type of chart: linear plot
+		type: 'line',
+
+		// Dataset: 'xdata' as labels, 'ydata' as dataset.data
+		data: {
+			labels: xdata,
+			datasets: [{
+				fill: false,
+				label: 'Roll',
+				backgroundColor: 'rgb(255, 0, 0)',
+				borderColor: 'rgb(255, 0, 0)',
+				data: ydataR,
+				lineTension: 0
+			},
+			{
+				fill: false,
+				label: 'Pitch',
+				backgroundColor: 'rgb(0, 255, 0)',
+				borderColor: 'rgb(0, 255, 0)',
+				data: ydataP,
+				lineTension: 0
+			},
+			{
+				fill: false,
+				label: 'Yaw',
+				backgroundColor: 'rgb(0, 0, 255)',
+				borderColor: 'rgb(0, 0, 255)',
+				data: ydataY,
+				lineTension: 0
+			}
+			]
+		},
+
+		// Configuration options
+		options: {
+			responsive: true,
+			maintainAspectRatio: false,
+			animation: false,
+			scales: {
+				yAxes: [{
+					scaleLabel: {
+						display: true,
+						labelString: 'Random value'
+					}
+				}],
+				xAxes: [{
+					scaleLabel: {
+						display: true,
+						labelString: 'Time [s]'
+					}
+				}]
+			}
+		}
+	});
+	
+	ydataR = chart.data.datasets[0].data;
+	ydataP = chart.data.datasets[1].data;
+	ydataY = chart.data.datasets[2].data;
+	xdata = chart.data.labels;
+	
+
+}
+
+$(document).ready(() => { 
+	chartInit();
+	$("#start").click(startTimer);
+	$("#stop").click(stopTimer);
+});
---
+++ IoT-System-Web/js/ledmatrix.js
@@ -0,0 +1,133 @@
+const getdata_url = "http://192.168.0.101/cgi-bin/get_pixels.py";
+const setdata_url = "http://192.168.0.101/cgi-bin/led_display.py";
+
+var currentLedState = new Array(64);
+var displayLedState = new Array(64);
+
+function checkIfChanged(){
+	for(let i = 0; i < displayLedState.length; i++){
+		let test = currentLedState[i][0];
+		let test2 = displayLedState[i][0];
+		if( currentLedState[i][0] != displayLedState[i][0] || currentLedState[i][1]!= displayLedState[i][1] || currentLedState[i][2]!= displayLedState[i][2]){
+			return true;
+		}
+	}
+	return false;
+}
+
+function updateStatus(){
+	if(checkIfChanged()){
+		$("#status").text("UNSAVED CHANGES*");
+	}
+	else {
+		$("#status").text("");
+	}
+}
+
+function changeColor() {
+	let r = $("#seekbarR").val();
+	let g = $("#seekbarG").val();
+	let b = $("#seekbarB").val();
+	let color = "rgb(" + r + ", " + g + ", " + b + ")";
+	$(this).css("background-color", color);
+	let id = $(this).attr('id').split('_')[1]
+	displayLedState[id][0] = r;
+	displayLedState[id][1] = g;
+	displayLedState[id][2] = b;
+	updateStatus();
+}
+
+function clearAll() {
+	for (var i=0; i < 64; i++) {
+		$("#led_"+i).css("background-color", "#000000");
+		currentLedState[i] = [0, 0 ,0];
+		displayLedState[i] = [0, 0 ,0];
+	}
+	updateStatus();
+	setLeds();
+}
+
+function initDisplay(responseJSON){
+	let r, g, b, color;
+	let row, col;
+	for(let i=0; i<responseJSON.length; i++){
+		currentLedState[i] = [...responseJSON[i]];
+		displayLedState[i] = [...responseJSON[i]];
+		r = responseJSON[i][0];
+		g = responseJSON[i][1];
+		b = responseJSON[i][2];
+		color = "rgb(" + r + ", " + g + ", " + b + ")";
+		let btn = $("<input>", {
+			id: "led_"+i, 
+			type: "button",
+			"class":"btn btn-default form-control"
+		});
+		btn.css("background-color", color);
+		btn.css("border", "1px solid #000000");
+		btn.click(changeColor);
+		if(i % 8 == 0){
+			row = $("<div>",{"class":"row pad_top"});
+		}
+		let col = $("<div>",{"class":"col", "id":"led_div_"+i});					
+		let input_group = $("<div>",{"class":"input-group"});
+		input_group.append(btn);
+		col.append(input_group);
+		row.append(col);
+		$("#led_matrix").append(row);
+	}
+}
+
+function sendData() {
+	let x, y, id;
+	let r, g, b;
+	let data = new Array(5);
+	let obj = {};
+	for(let i=0; i< displayLedState.length; i++){
+	data = new Array(5);
+	x = i % 8;
+	y = Math.floor(i/8);
+	id = "LED" + x + y;
+
+	r = displayLedState[i][0];
+	g = displayLedState[i][1];
+	b = displayLedState[i][2];
+	
+	currentLedState[i][0] = r;
+	currentLedState[i][1] = g;
+	currentLedState[i][2] = b;
+	
+	data[0] = x;
+	data[1] = y;
+	data[2] = r;
+	data[3] = g;
+	data[4] = b;
+	obj[id] = JSON.stringify(data);
+	}
+	return obj;
+}
+
+
+function setLeds() {
+	params = sendData();
+	console.log(params);
+	$.post(setdata_url, params, function(data){
+		console.log(data);
+	});
+	
+	updateStatus();
+}
+
+function init() {
+	$.ajax(getdata_url, {
+	type: 'GET', dataType: 'json',
+	success: function(responseJSON, status, xhr) {
+		initDisplay(responseJSON);
+	}
+	});
+};
+
+$(document).ready(() => { 
+	init();
+	$("#send").click(setLeds);
+	$("#clear").click(clearAll);
+});
