8c3fd10bdc4b720a67bca414d0afda7fbc878149
myszol321 <myszola321@wp.pl>
2021-06-23 21:04:40 +0200

    Update save_params.php

--- ServerScripts/save_params.php
+++ ServerScripts/save_params.php
@@ -2,12 +2,8 @@
 
 header("Content-Type: text/html");
 
-if(isset($_POST["port"])){
-	$port = $_POST["port"];
-}
-
-if(isset($_POST["api"])){
-	$api = $_POST["api"];
+if(isset($_POST["ip"])){
+	$ip = $_POST["ip"];
 }
 
 if(isset($_POST["sample_time"])){
@@ -18,5 +14,5 @@ if(isset($_POST["sample_amount"])){
 	$sample_amount = $_POST["sample_amount"];
 }
 
-$result = array("port" => $port, "api" => $api, "sample_time" => $sample, "sample_amount" => $sample_amount);
+$result = array("ip" => $ip, "sample_time" => $sample, "sample_amount" => $sample_amount);
 file_put_contents("config.json", json_encode($result));
